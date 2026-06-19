08092f067d20f90a323db179ff64267444d8d505
git <ctrl+p>
2026-06-19 13:09:18 +0800

    feat:added words

--- app/data.ts
+++ app/data.ts
@@ -1,5 +1,5 @@
 export const vocab = [
-    {korean:"아녕하세요", chinese: "你好"},
+    {korean:"아녕하세요", chinese: "您好"},
     {korean:"감사합니다", chinese: "謝謝"},
     {korean:"사랑해요", chinese: "我愛你"},
     {korean:"괜잖아요", chinese: "沒關係"},
@@ -8,5 +8,10 @@ export const vocab = [
     {korean:"내일", chinese: "明天"},
     {korean:"오늘", chinese:"今天"},
     {korean:"주말", chinese:"周末"},
-    {korean:"생일", chinese:"生日"}
+    {korean:"생일", chinese:"生日"},
+    {korean:"안녕히계세요", chinese:"再見（對方留下）"},
+    {korean:"알겠습니다", chinese:"我明白了"},
+    {korean:"잘부탁드립니다", chinese:"請多指教"},
+    {korean:"잠깐만요", chinese:"請稍等"},
+    {korean:"죄송합니다", chinese:"非常抱歉"}
 ]
